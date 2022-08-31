58b3d9c6da4c2e1ebe047d18e99f129e3239c847
aliray786 <66531634+aliray786@users.noreply.github.com>
2022-08-31 22:51:45 +0530

    Time: 131 ms (17.93%), Space: 30 MB (10.75%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -30,19 +30,49 @@ public:
         }
         return ;
     }
+    void bfs(vector<vector<int>>& heights,vector<vector<bool> >& visited,int i ,int j)
+    {
+        queue<pair<int,int>>q;
+        q.push({i,j});
+        while(!q.empty())
+        {
+            tie(i,j)=q.front(),q.pop();
+            if(visited[i][j])continue;
+            visited[i][j]=true;
+            if(pacific[i][j] and atlantic[i][j])
+                ans.push_back({i,j});
+             if(i+1<row and heights[i+1][j]>=heights[i][j])
+        {
+            q.push({i+1,j});
+        }
+        if(j+1<col and heights[i][j+1]>=heights[i][j])
+        {
+            q.push({i,j+1});
+        }
+        if(i-1>=0 and heights[i-1][j]>=heights[i][j])
+        {
+                        q.push({i-1,j});
+        }
+        if(j-1>=0 and heights[i][j-1]>=heights[i][j])
+        {
+                     q.push({i,j-1});
+
+        }
+        }
+    }
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         row=heights.size(),col=heights[0].size();
         pacific=atlantic=vector<vector<bool> >(row, vector<bool>(col, false));
         
         for(int j=0;j<col;j++)
         {
-            dfs(heights,pacific,0,j);
-            dfs(heights,atlantic ,row-1,j);
+            bfs(heights,pacific,0,j);
+            bfs(heights,atlantic ,row-1,j);
         }
          for(int i=0;i<row;i++)
         {
-            dfs(heights,pacific,i,0);
-            dfs(heights,atlantic ,i,col-1);
+            bfs(heights,pacific,i,0);
+            bfs(heights,atlantic ,i,col-1);
         }
         return ans;
         

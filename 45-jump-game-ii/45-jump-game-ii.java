d316d6e8c17badc414d2b8d1507d6d20bb4d23bf
aliray786 <66531634+aliray786@users.noreply.github.com>
2022-09-21 03:32:11 +0530

    Time: 9 ms (45.91%), Space: 49.6 MB (41.15%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,19 +1,17 @@
 class Solution {
-    int [] dp;
-    int minJump(int[]nums,int pos)
-    {
-        if(pos>=nums.length-1) return 0;
-        if(dp[pos]!=-1) return dp[pos];
-        int minJumps=10001;
-        for(int i=1;i<=nums[pos];i++)
-        {
-            minJumps=Math.min(minJumps,1+minJump(nums,pos+i));
-        }
-        return dp[pos]=minJumps;
-    }
+   
     public int jump(int[] nums) {
-        dp=new int[nums.length+1];
-        for(int i=0;i<nums.length;i++)dp[i]=-1;
-        return minJump(nums,0);
+       int jump=0,curEnd=0,farthestJump=0;
+        for(int i=0;i<nums.length-1;i++)
+        {
+            farthestJump=Math.max(farthestJump,i+nums[i]);
+            if(i==curEnd)
+            {
+                jump++;
+                curEnd=farthestJump;
+            }
+        }
+        return jump;
+        
     }
 }

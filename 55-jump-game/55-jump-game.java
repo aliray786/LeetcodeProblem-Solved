440c9f4d16ceb8130f3f11a4f6d1032e21561296
aliray786 <66531634+aliray786@users.noreply.github.com>
2022-09-20 23:25:17 +0530

    Time: 245 ms (18.67%), Space: 70.1 MB (5.54%) - LeetHub

---
+++ 55-jump-game/55-jump-game.java
@@ -0,0 +1,26 @@
+class Solution {
+    int[]mem;
+    int canReach(int[]nums,int pos)
+    {
+        if(pos==nums.length-1)
+        {
+            return 1;
+        }
+        if(mem[pos]!=-1) return mem[pos];
+        int farthestJump=Math.min(nums.length-1,pos+nums[pos]);
+        if(farthestJump==nums.length-1) return 1;
+        for(int i=pos+1;i<=farthestJump;i++)
+        {
+            if(canReach(nums,i)==1)
+            {
+                return mem[i]=1;
+            }
+        }
+        return mem[pos]=0;
+    }
+    public boolean canJump(int[] nums) {
+        mem=new int[nums.length+1];
+        for(int i=0;i<nums.length+1;i++)mem[i]=-1;
+        return canReach(nums,0)==1?true:false;
+    }
+}

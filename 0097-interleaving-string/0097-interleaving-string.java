c968a818fc9f5ca69cf59dd2f0743d085818908f
aliray786 <66531634+aliray786@users.noreply.github.com>
2022-10-18 21:18:17 +0530

    Time: 9 ms (44.59%), Space: 42 MB (74.75%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,42 +1,36 @@
 class Solution {
     int len1,len2,len3;
-    String s1,s2,s3;
-    Map<String,Boolean>mp=new HashMap<>();
-    boolean isPos(int p1,int p2,int p3)
-    {
-        if(p3==len3)
-        {
-            return p1==len1 && p2==len2;
-        }
-        String key=p1+"*"+p2;
-        if(mp.containsKey(key))
-            return mp.get(key);
-       else if(p1==len1)
-        {
-            return s2.charAt(p2)==s3.charAt(p3)?isPos(p1,p2+1,p3+1):false;
-        }
-        else if(p2==len2)
-        {
-            return s1.charAt(p1)==s3.charAt(p3)?isPos(p1+1,p2,p3+1):false;
-        }
-        boolean first=false,second=false;
-        if(s1.charAt(p1)==s3.charAt(p3))
-        {
-            first=isPos(p1+1,p2,p3+1);
-        }
-        if(s2.charAt(p2)==s3.charAt(p3))
-        {
-            second=isPos(p1,p2+1,p3+1);
-        }
-        mp.put(key,(first ||second));
-        return first || second;
-    }
     public boolean isInterleave(String s1, String s2, String s3) {
         len1=s1.length();
         len2=s2.length();
         len3=s3.length();
-        this.s1=s1;this.s3=s3;this.s2=s2;
-        if(len1+len2!=len3) return false;
-        return isPos(0,0,0);
+                if(len1+len2!=len3) return false;
+
+        boolean dp[][]=new boolean[len1+1][len2+1];
+        int p3=0;
+        for(int p1=0;p1<=len1;p1++)
+        {
+            for(int p2=0;p2<=len2;p2++)
+            {
+                p3=p1+p2-1;
+                if(p1==0 && p2==0 )
+                {
+                    dp[p1][p2]=true;
+                }
+                else if(p1==0)
+                {
+                    dp[p1][p2]=s2.charAt(p2-1)==s3.charAt(p3) && dp[p1][p2-1];
+                }
+                else if(p2==0)
+                {
+                    dp[p1][p2]= s1.charAt(p1-1)==s3.charAt(p3) && dp[p1-1][p2];
+                }
+                else
+                {
+                    dp[p1][p2]=(s1.charAt(p1-1)==s3.charAt(p3) && dp[p1-1][p2] )|| (s2.charAt(p2-1)==s3.charAt(p3)&&dp[p1][p2-1]);
+                }
+            }
+        }
+        return dp[len1][len2];
     }
 }
